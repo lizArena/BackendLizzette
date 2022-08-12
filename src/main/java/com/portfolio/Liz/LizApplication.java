7c40d366e7611a57292a34f122187610fb1864ca
NaddMija <lizarbieto1234@gmail.com>
2022-08-12 15:41:46 -0300

    se empieza con el backend

---
+++ src/main/java/com/portfolio/Liz/LizApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Liz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/Liz/LizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Liz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
