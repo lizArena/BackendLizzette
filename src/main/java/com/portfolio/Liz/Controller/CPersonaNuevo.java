9f70c5cd3416591fb1cba19274501d1df263692d
NaddMija <lizarbieto1234@gmail.com>
2022-08-15 01:47:05 -0300

    primer cambio

--- src/main/java/com/portfolio/Liz/Controller/CPersonaNuevo.java
+++ src/main/java/com/portfolio/Liz/Controller/CPersonaNuevo.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/personaNuevo")
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://front-lizcruz-firebase.web.app/")
 public class CPersonaNuevo {
     
     @Autowired
